f75c3204d23b516fbf1db1503d09ee6a841eaf0c
Morteza <mortazapor3@gmail.com>
2023-06-11 10:48:05 +0330

    Improve website reactivity

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,14 @@
-function showMenu (){
-    var menu = document.getElementById("menu")
-    if (menu.className === "header-menu"){
-        menu.className += " show";
-    }
-    else{
-        menu.className = "header-menu"
-    }
+function showMenu() {
+	if (menu.className === "header-menu") {
+		menu.className += " show";
+		main.classList.add("blur");
+		footer.classList.add("blur");
+		burger.style.position = "absolute";
+		burger.style.zIndex = 11;
+	} else {
+		menu.className = "header-menu";
+		main.classList.remove("blur");
+		footer.classList.remove("blur");
+		burger.style.position = "";
+	}
 }
